299aa3a4269bb76eba014f8d0484ed62b5192726
Tuan Nguyen Gia <tuanng1126@gmail.com>
2022-10-12 15:17:40 +0700

    Add tests

---
+++ tests/test_users.py
@@ -0,0 +1,34 @@
+import pytest
+from jose import jwt
+from app import schemas
+
+from app.config import settings
+
+
+# def test_root(client):
+
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message') == 'Hello World'
+#     assert res.status_code == 200
+
+
+def test_create_user(client):
+    res = client.post(
+        "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+
+    new_user = schemas.UserResponse(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
+
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/auth/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                         settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "Bearer"
+    assert res.status_code == 200
